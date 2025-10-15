35a0e119f50841acb5e1097c0c97cad0f20aca0a
amanda-huggenkiss <thanhnam.lhper.9@gmail.com>
2025-10-15 23:55:04 +0700

    1.0200902

--- scripts/text_trainer.py
+++ scripts/text_trainer.py
@@ -200,7 +200,7 @@ def main():
     )
     
     parser.add_argument(
-        "--reg-ratio", type=float, help="Reg ratio to use for training", default=1.0180201
+        "--reg-ratio", type=float, help="Reg ratio to use for training", default=1.0200902
     )
     
     args = parser.parse_args()
